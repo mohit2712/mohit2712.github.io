0ffbbf4aed1a978a3e1e7a3b99a8bb08a202851f
Mohit Agrawal <mohit.agrawal2712@gmail.com>
2017-06-09 15:19:45 +0530

    Create recaptcha.php

--- recaptcha.php
+++ recaptcha.php
@@ -6,6 +6,6 @@
 </script>
 
 <form id="contact" action="submitform.php" method="post">
-	<label>Name : <input type="text" name="field"></label>
+	<label>Name : <input type="text"></label>
 	<input type="submit" class="g-recaptcha" data-sitekey="6LdaLSQUAAAAAMa70ISBGq78YICdg42J2JMLXsne" data-callback="captchaSubmit" name="Submit" />
 </form>
